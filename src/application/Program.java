d26a5058f99de5f4295c315cb856fe2b33604a7a
TonyALS <tony_luzsilva@hotmail.com>
2019-07-09 19:29:57 -0300

    Insere método limpa tela na classe UI

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -9,22 +11,31 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		ChessMatch partidaChadrez = new ChessMatch();
-		
-		while(true) {
-			UI.imprimeTabuleiro(partidaChadrez.getPecas());
-			System.out.println();
-			System.out.print("Origem: ");
-			ChessPosition origem = UI.lerPosicaoXadrez(sc);
-			
-			System.out.println();
-			System.out.print("Destino: ");
-			ChessPosition destino = UI.lerPosicaoXadrez(sc);
-			
-			ChessPiece pecaCapturada = partidaChadrez.movimentaPeca(origem, destino);
+
+		while (true) {
+			try {
+				UI.limparTela();
+				UI.imprimeTabuleiro(partidaChadrez.getPecas());
+				System.out.println();
+				System.out.print("Origem: ");
+				ChessPosition origem = UI.lerPosicaoXadrez(sc);
+
+				System.out.println();
+				System.out.print("Destino: ");
+				ChessPosition destino = UI.lerPosicaoXadrez(sc);
+
+				ChessPiece pecaCapturada = partidaChadrez.movimentaPeca(origem, destino);
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
-		
 	}
 }
--- src/application/UI.java
+++ src/application/UI.java
@@ -30,6 +30,12 @@ public class UI {
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	//-----------------------------------------------------------------
 	
+	//Código para limpar tela:
+	public static void limparTela() {
+		System.out.println("\033[H\033[2J");
+		System.out.flush();
+	}
+	
 	public static ChessPosition lerPosicaoXadrez(Scanner sc) {
 		try {
 		String s = sc.nextLine();

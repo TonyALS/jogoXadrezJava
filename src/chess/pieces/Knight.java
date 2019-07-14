1fa2486509b690772d03d10ab1ecd28d39567226
TonyALS <tony_luzsilva@hotmail.com>
2019-07-14 11:40:45 -0300

    Knight - Cavalo

---
+++ src/chess/pieces/Knight.java
@@ -0,0 +1,73 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Knight extends ChessPiece {
+
+	public Knight(Board tabuleiro, Color cor) {
+		super(tabuleiro, cor);
+	}
+
+	@Override
+	public String toString() {
+		return "N";
+	}
+
+	private boolean podeMover(Position posicao) {
+		ChessPiece p = (ChessPiece) getTabuleiro().peca(posicao);
+		return p == null || p.getCor() != getCor();
+	}
+
+	@Override
+	public boolean[][] movimentosPossiveis() {
+		// Cria uma matriz de boolean com tamanho do tabuleiro.
+		boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+
+		Position p = new Position(0, 0);
+
+		p.setValor(posicao.getLinha() - 1, posicao.getColuna() - 2);
+		if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		p.setValor(posicao.getLinha() - 2, posicao.getColuna() - 1);
+		if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		p.setValor(posicao.getLinha() - 2, posicao.getColuna() + 1);
+		if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		p.setValor(posicao.getLinha() - 1, posicao.getColuna() + 2);
+		if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		p.setValor(posicao.getLinha() + 1, posicao.getColuna() + 2);
+		if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		p.setValor(posicao.getLinha() + 2, posicao.getColuna() + 1);
+		if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		p.setValor(posicao.getLinha() + 2, posicao.getColuna() - 1);
+		if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		p.setValor(posicao.getLinha() + 1, posicao.getColuna() - 2);
+		if (getTabuleiro().posicaoExiste(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		return mat;
+	}
+}
